2cbb9aa11cc6c3a47edd606d6d50d0c636779764
Kseniya <pupkoksenia@gmail.com>
2024-04-15 01:53:26 +0300

    main

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app" class="flex items-center justify-center h-screen bg-gray-100">
+    <HelloWorld />
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+  },
+};
+</script>
---
+++ src/main.js
@@ -0,0 +1,5 @@
+import { createApp } from "vue";
+import App from "./App.vue";
+import "../src/assets/app.css";
+
+createApp(App).mount("#app");
